d760975c7655ccd306486d96ba9537a48efe271c
Lalit <lalitvijay9480@gmail.com>
2021-06-01 07:58:50 +0530

    Apply prettier changes

--- script.js
+++ script.js
@@ -3,19 +3,21 @@
 
 // prints "hi" in the browser's dev tools console
 setInterval(() => {
-    const timer = new Date();
-    let mins = timer.getMinutes();
+  const timer = new Date();
+  let mins = timer.getMinutes();
 
-    let minutesLeft = mins % 15;
+  let minutesLeft = mins % 15;
 
-    if (minutesLeft == 0) {
-        // console.log('Hiiii');
-        var audio = new Audio('https://cdn.glitch.com/3f341d4f-9097-4a04-84df-c420f0b63127%2Fwatersound.mp3?v=1604661569707');
+  if (minutesLeft == 0) {
+    // console.log('Hiiii');
+    var audio = new Audio(
+      "https://cdn.glitch.com/3f341d4f-9097-4a04-84df-c420f0b63127%2Fwatersound.mp3?v=1604661569707"
+    );
 
-        audio.play();
-    } else {
-        minutesLeft = 15 - minutesLeft;
-    }
+    audio.play();
+  } else {
+    minutesLeft = 15 - minutesLeft;
+  }
 
-    document.querySelector('#time').textContent = `${minutesLeft} minutes left`;
-}, 60000)
+  document.querySelector("#time").textContent = `${minutesLeft} minutes left`;
+}, 60000);
